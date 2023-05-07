594e5ab8db4decb98377ac4cf486c31fb50120ed
WebPerformer <webperformerz@gmail.com>
2023-05-07 20:12:44 -0300

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 
 import Logo from "../../public/images/aiface.png"
+
 import { Bebas_Neue } from 'next/font/google'
 
 const bebas = Bebas_Neue({
@@ -12,18 +13,20 @@ const bebas = Bebas_Neue({
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-screen p-6 flex items-center justify-between">
 
-      <div>
-        <h1 className={bebas.className}>FaceSegura</h1>
-        <p>A segurança feito com a identificação de seu rosto</p>
-        <div>
-          <button>Entrar</button>
-          <button>Cadastro</button>
+      <div className="w-1/2">
+        <h1 className={`${bebas.className} text-9xl`}>FaceSegura</h1>
+        <p className="text-6xl mb-10">A segurança feito com a identificação de seu rosto</p>
+        <div className="flex items-center gap-5">
+          <button className="px-20 py-5 text-4xl bg-primary">Entrar</button>
+          <button className="px-20 py-5 text-4xl bg-primary">Cadastro</button>
         </div>
       </div>
 
-      <div></div>
+      <div>
+        <Image src={Logo} alt=""/>
+      </div>
 
     </div>
   )
