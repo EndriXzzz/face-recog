343a400fc731b8b205082cbbf189b51889d3d2ef
Ericd <ericdom5.ed@gmail.com>
2023-05-07 19:27:27 -0300

    Tela Inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image"
 
+import Logo from "../../public/images/aiface.png"
+import { Bebas_Neue } from 'next/font/google'
+
+const bebas = Bebas_Neue({
+  weight: ["400"],
+  style: "normal",
+  subsets: ["latin"],
+})
+
+
 export default function Home() {
   return (
     <div>
 
       <div>
-        <h1></h1>
-        <p></p>
+        <h1 className={bebas.className}>FaceSegura</h1>
+        <p>A segurança feito com a identificação de seu rosto</p>
         <div>
-          <button></button>
-          <button></button>
+          <button>Entrar</button>
+          <button>Cadastro</button>
         </div>
       </div>
 
