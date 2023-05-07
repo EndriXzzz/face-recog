f51a3992a7d31d2f96bcf6305b46d0a965a341be
WebPerformer <webperformerz@gmail.com>
2023-05-07 18:50:07 -0300

    typescript template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'light': '#FFFFFF',
+      'dark': '#18181B',
+      'primary': '#2AE98C',
+      'darkness': '#38383C',
+      'gray': '#818181',
+      'transparent': 'rgba(0, 0, 0, 0)'
     },
+    screens: {
+      'xl': {'raw': '(max-width: 1440px)'},
+      'lg': {'raw': '(max-width: 1024px)'},
+      'md': {'raw': '(max-width: 768px)'},
+      'sl': {'raw': '(max-width: 425px)'},
+      'sm': {'raw': '(max-width: 375px)'},
+      'xs': {'raw': '(max-width: 320px)'},
+      'h-md': {'raw': '(max-height: 700px)'}
+    }
   },
   plugins: [],
 }
